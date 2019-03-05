9a3206c20c67644895a9575c28b97c62d7de35d3
viktor <viktor_sjr@163.com>
2019-03-05 19:32:15 +0800

    格式更新！

--- src/main/java/cn/itcast/maven/quickstart/App.java
+++ src/main/java/cn/itcast/maven/quickstart/App.java
@@ -9,8 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "第一次修改失败！" );
-	System.out.println( "第二次修改！" );
-	System.out.println( "第三次修改！" );
+		System.out.println( "第一次修改失败！" );
+		System.out.println( "第二次修改！" );
+		System.out.println( "第三次修改！" );
     }
 }
