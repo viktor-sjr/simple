bf4cc132efda414b390121670392567b0d331322
viktor <viktor_sjr@163.com>
2019-03-05 19:12:15 +0800

    修改
    第二次修改！

--- src/main/java/cn/itcast/maven/quickstart/App.java
+++ src/main/java/cn/itcast/maven/quickstart/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		System.out.println( "第一次修改成功！" );
+		System.out.println( "第一次修改失败！" );
+		System.out.println( "第二次修改！" );
     }
 }
